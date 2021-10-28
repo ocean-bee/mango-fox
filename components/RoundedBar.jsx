4ff54487a59c155df395e16f87cb690d18ffa547
Kalani Akita <26021752+ocean-bee@users.noreply.github.com>
2021-10-27 16:09:49 -1000

    update colors

--- components/RoundedBar.jsx
+++ components/RoundedBar.jsx
@@ -2,7 +2,7 @@ export default function RoundedBar ({title}) {
     return(
         <div>
             <div className="bg-black w-min px-3 py-2 rounded-xl shadow-xl whitespace-nowrap">
-                <p className="text-2xl font-mono text-pink-600 uppercase">{title}</p>
+                <p className="text-2xl font-mono text-gray-200 uppercase">{title}</p>
             </div>
         </div>
     )
